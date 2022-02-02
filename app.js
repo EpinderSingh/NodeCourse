7bd9bbe78a6c74b230b481e6df6401b94f637af4
Epinder Singh Mann <epinder.singh@gmail.com>
2022-02-02 10:38:15 +0800

    POST request middelware

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 //Third party modules
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/add-product', (req, res, next) => {
-  console.log('In  middelware');
-  res.send('<h1>Hello from add-products PAGE!</h1>');
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>',
+  );
+});
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body.title);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('In ANOTHER middelware');
   res.send('<h1>Hello from Express!</h1>');
 });
 
